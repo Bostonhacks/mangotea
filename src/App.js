ef6074f82e762f019091da15abfda1f8d0a805da
Tin Nguyen <tinhoang@bu.edu>
2020-04-27 01:57:54 -0400

    restyled nav using bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 import {
   Route,
@@ -7,10 +6,14 @@ import {
   BrowserRouter as Router,
   Link,
 } from 'react-router-dom'
+
 import './styles/Router.css'
 import './pages/Home/components/Home.css'
+
 import Home from './pages/Home/components/Home'
 import Organizer from './pages/Organizer'
+
+import BostonHacks from './images/bostonhackslogo.jpg'
 
 // TODO: Set up react router
 //       Work on About/Contact and Organizer pages
@@ -18,35 +21,30 @@ import Organizer from './pages/Organizer'
 function App() {
   return (
     <Router>
-      <div className="router-div">
-        <ul>
-          <li>
-            <img src={logo} className="App-logo" alt="logo"/>
-          </li>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-
-          <li>
-            <Link to="/organizer">Organizers</Link>
-          </li>
-
+        <ul id="nav" class="nav nav-pills">
+            <li class="nav-item"> 
+              <img src={BostonHacks} className="router-logo" alt="logo"/>
+            </li>
+            <li class="nav-item">
+                <Link class="nav-link" to="/">Home</Link>
+            </li>
+            <li class="nav-item">
+                <Link class="nav-link" to="/about">About</Link>
+            </li>
+            <li class="nav-item">
+                <Link class="nav-link" to="/contact">Contact</Link>
+            </li>
+            <li class="nav-item">
+                <Link class="nav-link" to="/organizer">Organizers</Link>
+            </li>
         </ul>
-        <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route path="/about" component={Home}/>
-          <Route path="/contact" component={Home}/>
-          <Route path="/organizer" component={Organizer}/>
-
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={Home}/>
+            <Route path="/contact" component={Home}/>
+            <Route path="/organizer" component={Organizer}/>
+          </Switch>
+      </Router>
   );
 }
 
