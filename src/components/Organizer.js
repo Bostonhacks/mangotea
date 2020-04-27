fc75ab84a88704f0e0d2d1156343db3297088c20
Tin Nguyen <tinhoang@bu.edu>
2020-04-27 01:12:27 -0400

    centered divs on organizer page using bootstrapped cards

--- src/components/Organizer.js
+++ src/components/Organizer.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const OrganizerBlock = ({ data }) => (
-    <div class="card" style={{width: '10rem', margin: "1rem"}}>
-        <img src={require(`../images/${data.image}`)} class="card-img-top" alt="..."/>
-        <div class="card-body">
-            <h5 class="card-title">{data.name} </h5>
+    <div className="card organizer-card" style={{width: '10rem', margin: "1rem"}}>
+        <img src={require(`../images/${data.image}`)} className="card-img-top" alt="..."/>
+        <div className="card-body">
+            <h5 className="card-title">{data.name} </h5>
             <p> {data.committee} </p>
         </div>
     </div>
